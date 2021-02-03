1976c83b48e48f5b6b68ffc64ecbd3ad6de85824
rroy7631 <70105675+rroy7631@users.noreply.github.com>
2021-02-03 12:22:11 +0530

    Add files via upload

---
+++ assignment3.js
@@ -0,0 +1,54 @@
+// Question 1
+// Write a JavaScript program to display the reading status (i.e. display book name, author name and
+// reading status) of the following books
+
+// Output should look like this:
+// Already read 'Bill Gates' by The Road Ahead.
+// Already read 'Steve Jobs' by Walter Isaacson.
+// You still need to read 'Mockingjay: The Final Book of The Hunger Games' by Suzanne Collins.
+
+
+
+
+// var library = [
+//     {
+//     author: 'Bill Gates',
+//     title: 'The Road Ahead',
+//     readingStatus: true
+//     },
+//     {
+//     author: 'Steve Jobs',
+//     title: 'Walter Isaacson',
+//     readingStatus: true
+//     },
+//     {
+//     author: 'Suzanne Collins',
+//     title: 'Mockingjay: The Final Book of The Hunger Games',
+//     readingStatus: false
+//     }];
+
+
+
+//     for(var i=0;i<library.length;i++)
+//     {
+//         if (library[i].readingStatus === true) {
+//             console.log("Already read '"  +  library[i].author + "' by", library[i].title + ".");
+//         } else {
+//             console.log("You still need to read '" + library[i].title + "' by", library[i].author + ".");
+//         }
+//     }
+
+
+    // Write a javascript program that generates an alert "Not legal age to drive" if the driver age is below 18
+    // years old, if driver age is greater than or equal to 18, it generates an alert "Drive safe".
+
+
+   
+    
+// var age=prompt("Enter ur age: ");
+// if(age>=18){
+//     alert("Drive safe");
+//     location ="https://www.crazygames.com/t/bike";
+// }else{
+//     alert("Not legal age to drive");
+// }
